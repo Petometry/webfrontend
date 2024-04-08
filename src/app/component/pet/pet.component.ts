80ca1ebe2d64d70039ec939a1dafce58e28242f1
roboter5123 <pascal.roboter@hotmail.com>
2024-04-08 21:13:54 +0200

    Fix for slow rendering of pets

--- src/app/component/pet/pet.component.ts
+++ src/app/component/pet/pet.component.ts
@@ -1,4 +1,14 @@
-import {afterNextRender, AfterRenderPhase, Component, ElementRef, input, ViewChild} from '@angular/core';
+import {
+  afterNextRender,
+  afterRender,
+  AfterRenderPhase,
+  AfterViewInit,
+  ChangeDetectorRef,
+  Component,
+  ElementRef,
+  input,
+  ViewChild
+} from '@angular/core';
 import {NgStyle} from "@angular/common";
 import {Petoverview} from "../../model/pet/petoverview";
 
@@ -11,7 +21,7 @@ import {Petoverview} from "../../model/pet/petoverview";
   templateUrl: './pet.component.html',
   styleUrl: './pet.component.css'
 })
-export class PetComponent{
+export class PetComponent implements AfterViewInit {
 
   pet = input.required<Petoverview>();
 
@@ -23,20 +33,28 @@ export class PetComponent{
   private resizeObserver: ResizeObserver | null = null;
 
 
-  constructor() {
+  constructor(private cd: ChangeDetectorRef) {
+    afterRender(() => this.calculatePetSize())
     afterNextRender(() => {
       this.resizeObserver = new ResizeObserver(() => {
         this.calculatePetSize();
       });
-
       this.resizeObserver.observe(this.container.nativeElement);
     }, {phase: AfterRenderPhase.Write});
   }
 
+  ngAfterViewInit(): void {
+    // This updates pet size after everything has rendered by telling angular
+    // to rerun change detection after setting the size.
+    // This fixed the NG0100: Expression has changed after it was checked error message
+    this.calculatePetSize();
+    this.cd.detectChanges();
+  }
+
   private calculatePetSize() {
 
-    const containerWidth = this.container.nativeElement.clientWidth
-    const containerHeight = this.container.nativeElement.clientHeight
+    const containerWidth = this.container.nativeElement.clientWidth;
+    const containerHeight = this.container.nativeElement.clientHeight;
     let petSize = containerWidth > containerHeight ? containerHeight : containerWidth;
     this.petSize = petSize + "px";
   }
