8f3454458acede71fd3f949b3b0bc19f3ac8a709
roboter5123 <73991535+roboter5123@users.noreply.github.com>
2024-04-09 13:35:03 +0200

    Update activity.service.ts

--- src/app/service/activityservice/activity.service.ts
+++ src/app/service/activityservice/activity.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import {Observable} from "rxjs";
-import {Activity} from "../../model/activity/activity";
+import {Work} from "../../model/activity/work";
 import {HttpService} from "../httpservice/http.service";
 
 @Injectable({
@@ -10,14 +10,18 @@ export class ActivityService {
 
   constructor(private httpService:HttpService) { }
 
-  getCurrentActivity():Observable<Activity|undefined> {
+  getCurrentActivity():Observable<Work|undefined> {
 
     return this.httpService.sendGetRequest("activity", "activities")
   }
 
-  startWork(hoursToWork: number):Observable<Activity> {
+  startWork(hoursToWork: number):Observable<Work> {
     let work = {duration: hoursToWork}
     return this.httpService.sendPostRequest("activity", "activities/work", work)
+  }
+
+    getWork():Observable<Work|undefined> {
+    return this.httpService.sendGetRequest("activity", "activities/work")
   }
 
   stopCurrentActivity():Observable<undefined> {
