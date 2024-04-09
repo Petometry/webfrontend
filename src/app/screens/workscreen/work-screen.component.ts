6962c56b27db4b0b403fc27c27006993c7bae16e
roboter5123 <73991535+roboter5123@users.noreply.github.com>
2024-04-09 13:53:52 +0200

    Update work-screen.component.ts

--- src/app/screens/workscreen/work-screen.component.ts
+++ src/app/screens/workscreen/work-screen.component.ts
@@ -1,6 +1,7 @@
 import {AfterViewInit, Component, ViewChild} from '@angular/core';
 import {ActivityService} from "../../service/activityservice/activity.service";
 import {Work} from "../../model/activity/work";
+import {Activity} from "../../model/activity/activity";
 
 @Component({
   selector: 'app-workplace',
