b171e23d2818297adcc6181983482c729d0613b7
roboter5123 <73991535+roboter5123@users.noreply.github.com>
2024-04-10 15:42:15 +0200

    Update activity.reducers.ts

--- src/app/store/activity/activity.reducers.ts
+++ src/app/store/activity/activity.reducers.ts
@@ -1,9 +1,25 @@
-export const initialState: activityState = {
-activities: [],
+export interface ActivityState {
+activity: Activity;
+loading: boolean;
+error: string;
+}
+export const initialState: ActivityState = {
+activity: {} as Activity,
 loading: false,
 error: ''
 };
-export const activityReducer = createReducer(
+export const todoReducer = createReducer(
 initialState,
+
+on(TodoActions.loadTodos, state => ({ ...state, loading: true })),
+
+on(TodoActions.loadTodosSuccess, (state, { todos }) =>({ ...state, todos, loading: false })),
+
+on(TodoActions.loadTodosFailure, (state, { error }) => ({ ...state, error, loading: false })),
+
+on(TodoActions.addTodo, (state, { todo }) => ({ ...state, todos: [...state.todos, todo] })),
+
 on(TodoActions.updateTodo, (state, { todo }) => ({ ...state, todos: state.todos.map(t => t.id === todo.id ? todo : t) })),
+
+on(TodoActions.deleteTodo, (state, { id }) => ({ ...state, todos: state.todos.filter(t => t.id !== id) })),
 );
