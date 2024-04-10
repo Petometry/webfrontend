aec8e45378bca683cde4eb1e80013f0c61ea3947
roboter5123 <73991535+roboter5123@users.noreply.github.com>
2024-04-10 15:13:31 +0200

    Update pets.selector.ts

--- src/app/state/pets/pets.selector.ts
+++ src/app/state/pets/pets.selector.ts
@@ -1,1 +1,19 @@
+import { createFeatureSelector, createSelector } from '@ngrx/store';
+import { AppState } from '../app.state';
+import { PetOverview } from "../../models/pets/petoverview";
 
+// Get complete state of the favorites products in application
+export const selectAppState = createFeatureSelector<AppState>('pets');
+
+// get All pets
+export const selectProducts = createSelector(
+  selectAppState,
+  (state: AppState) => state.pets
+);
+
+// get One pet by ID
+export const selectProductById = createSelector(
+  selectProducts,
+  (pets: PetOverview[], props: { petId: number }) =>
+    pets.find(pet => pet.id === props.petId)
+);
