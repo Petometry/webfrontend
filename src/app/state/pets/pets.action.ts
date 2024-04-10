7b71482efbd12ad7442bb2b6d4e1da2e69af9b32
roboter5123 <73991535+roboter5123@users.noreply.github.com>
2024-04-10 14:50:15 +0200

    Update pets.action.ts

--- src/app/state/pets/pets.action.ts
+++ src/app/state/pets/pets.action.ts
@@ -1,8 +1,7 @@
 import { createAction, props } from '@ngrx/store';
 import { PetOverview } from "../../models/pet/petoverview";
 
-export const add = createAction('[FavoriteProduct] Add',  props<{ product: FavoriteProduct }>());
-export const remove = createAction('[FavoriteProduct] Remove', props<{ product: FavoriteProduct }>());
-export const updateAllState = createAction('[FavoriteProduct] Update all state of favorites products', 
- props<{ products: FavoriteProduct[] }>());
-export const clear = createAction('[FavoriteProduct] Clear');
+export const add = createAction('[PetOverview] Add',  props<{ pet: PetOverview }>());
+export const remove = createAction('[PetOverview] Remove', props<{ pet: PetOverview }>());
+export const updateAllState = createAction('[PetOverview] Update all state of Pets', props<{ products: PetOverview[] }>());
+export const clear = createAction('[PetOverview] Clear');
