c24ec7ec581666080f462299b1b4d9cb971cd9d6
roboter5123 <73991535+roboter5123@users.noreply.github.com>
2024-04-10 15:46:25 +0200

    Update activity.actions.ts

--- src/app/store/activity/activity.actions.ts
+++ src/app/store/activity/activity.actions.ts
@@ -3,5 +3,4 @@ import { createAction } from '@ngrx/store';
 export const loadActivity = createAction('[Activity] Load Activity');
 export const loadActivitySuccess = createAction('[Activity] Load Activity Success', props<{ activity: Activity }>());
 export const loadActivityFailure = createAction('[Activity] Load Activity Failure', props<{ error: string }>());
-export const updateActivity = createAction('[Activity] Update Activity', props<{ activity: Activity }>());
 export const deleteActivity = createAction('[Activity] Delete Activity', props<void>());
