b0d720338445cff7c692e0bf402d896f93e57afe
roboter5123 <73991535+roboter5123@users.noreply.github.com>
2024-04-10 15:08:05 +0200

    Update pets.reducer.ts

--- src/app/state/pets/pets.reducer.ts
+++ src/app/state/pets/pets.reducer.ts
@@ -1,1 +1,26 @@
+import { createReducer, on } from '@ngrx/store';
+import { add, remove, clear, updateAllState } from '/pets.action';
+import { AppState } from '../app.state';
 
+export const initialState: AppState = {
+  pets:[],
+};
+
+export const petsReducer = createReducer(
+  
+  initialState,
+  on(add, (state, {pet}) => ({
+      ...state,
+      pets: [...state.products, pet]
+    })),
+  
+  on(remove, (state, {pet}) => ({
+    ...state,
+    pets: state.pets.filter((p)=> pet.id != p.id)
+  })),
+  on(updateAllState, (state, {pets}) => ({
+      ...state,
+      pets
+  })),
+  on(clear, state => initialState)
+);
