1a0762d6969ea0d95d1eb4fb59e271a91f25e6e2
roboter5123 <pascal.roboter@hotmail.com>
2024-04-15 20:05:24 +0200

    Added effect for loading either work or foraging when activity was loaded

--- src/app/store/effects/activity.effects.ts
+++ src/app/store/effects/activity.effects.ts
@@ -3,6 +3,10 @@ import {Actions, createEffect, ofType} from "@ngrx/effects";
 import {loadActivity, loadActivityError, loadActivitySuccess} from "../actions/activity.actions";
 import {ActivityService} from "../../service/activityservice/activity.service";
 import {catchError, map, mergeMap, of} from "rxjs";
+import {loadForaging} from "../actions/foraging.actions";
+import {ActivityModel} from "../../model/activity/activity.model";
+import {loadWork} from "../actions/work.actions";
+import {TypedAction} from "@ngrx/store/src/models";
 
 @Injectable()
 export class ActivityEffects {
@@ -17,4 +21,20 @@ export class ActivityEffects {
         this.activityService.getCurrentActivity().pipe(map((activity) => loadActivitySuccess({activity})), catchError((error) => of(loadActivityError({error}))))
       ))
   )
+
+  loadActivitySuccess$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(loadActivitySuccess),
+      mergeMap((props) =>
+        of(props).pipe(map((props)=>this.mapLoadActivity(props.activity)))
+      ))
+  )
+
+  private mapLoadActivity(activity: ActivityModel):TypedAction<"[Work] Load Work"| "[Foraging] Load Foraging">   {
+    if (activity.type == "WORK"){
+      return loadWork();
+    }else {
+      return loadForaging()
+    }
+  }
 }
